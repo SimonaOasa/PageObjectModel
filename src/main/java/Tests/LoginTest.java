3efe8d3d854f07c6ed6bef818c49ac75d2744d76
simona oasa <simona.oasa@outlook.com>
2024-05-24 18:04:46 +0300

    curs24 pageObjectModel

---
+++ src/main/java/Tests/LoginTest.java
@@ -0,0 +1,45 @@
+package Tests;
+
+import static org.junit.Assert.assertTrue;
+
+import org.testng.annotations.Test;
+
+import Framework.Utils.PropertiesFileProcessor;
+import PageObjects.LoginPage;
+import PageObjects.MenuPage;
+import SeleniumUtils.BaseTest;
+
+
+public class LoginTest extends BaseTest{
+    
+	String USER = PropertiesFileProcessor.readPropertiesFile("user", "credentials.properties");
+	String PASS = PropertiesFileProcessor.readPropertiesFile("pass", "credentials.properties");
+	
+	@Test(priority=1)
+	public void loginTest() throws InterruptedException {
+		
+		MenuPage menu = new MenuPage(driver);
+		
+		menu.navigateTo(menu.loginLink);
+		LoginPage login = new LoginPage(driver);
+		login.loginInApp(USER, PASS);
+		Thread.sleep(2000);
+		assertTrue(login.checkMsgIsDisplayed(login.successLoginMsg));
+		login.logoutFromApp();
+	}
+	
+	@Test(priority=2)
+    public void invalidloginTest() throws InterruptedException {
+		
+		MenuPage menu = new MenuPage(driver);
+		
+		menu.navigateTo(menu.loginLink);
+		LoginPage login = new LoginPage(driver);
+		login.loginInApp(USER, "12312313");
+		Thread.sleep(2000);
+		assertTrue(login.checkMsgIsDisplayed(login.errorLoginMsg));
+		
+	}
+	
+	
+}
