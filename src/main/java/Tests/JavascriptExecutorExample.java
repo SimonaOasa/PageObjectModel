17ade4f7c36f61d4a002cb09ea18ef3dd9ea6af2
simona oasa <simona.oasa@outlook.com>
2024-06-18 16:48:04 +0300

    curs 30 jsexecutor part 2

--- src/main/java/Tests/JavascriptExecutorExample.java
+++ src/main/java/Tests/JavascriptExecutorExample.java
@@ -1,5 +1,8 @@
 package Tests;
 
+import static org.junit.Assert.assertTrue;
+import static org.testng.Assert.assertEquals;
+
 import org.openqa.selenium.JavascriptExecutor;
 import org.testng.annotations.Test;
 
@@ -7,7 +10,7 @@ import SeleniumUtils.BaseTest;
 
 public class JavascriptExecutorExample extends BaseTest{
 
-	@Test
+	//@Test
 	public void example1() {
 		
 		JavascriptExecutor jse = (JavascriptExecutor) driver;
@@ -31,9 +34,64 @@ public class JavascriptExecutorExample extends BaseTest{
 		*/ 
 		
 		jse.executeScript("document.getElementsByClassName('icon-search')[0].click();"
-				+ "document.getElementsByClassName('search_field')[0].value='cooking'"
+				+ "document.getElementsByClassName('search_field')[0].value='cooking';"
 				+ "document.getElementsByClassName('icon-search')[0].click();");
 		
+		//alternativa pt isDisplayed()
+		
+		String visible = jse.executeScript("return document.getElementsByClassName('post_title')[0].childNodes[0].checkVisibility()")
+				.toString();
+		
+		System.out.println(visible);
+		assertEquals(visible, "true");
+		
+		boolean isVisible = Boolean.parseBoolean(visible);
+		
+		assertTrue(isVisible);
+		
+		//alternativa pt get text 
+		
+		String bookTitle = jse.executeScript("return document.getElementsByClassName('post_title')[0].childNodes[0].innerText")
+		.toString();
+		
+		System.out.println(bookTitle);
+		
+		jse.executeScript("document.getElementsByClassName('post_title')[0].childNodes[0].click()");
+		
+		//alternativa pt get current url 
+		//driver.getCurrentUrl();
+		String currentUrl = jse.executeScript("return document.URL").toString();
+		System.out.println(currentUrl);
+		//alternativa pt get title 
+		String currentPageTitle = jse.executeScript("return document.title").toString();
+		System.out.println(currentPageTitle);
+		
+        jse.executeScript("window.scrollBy(0,800)");
+        jse.executeScript("document.getElementById('tab-title-reviews').children[0].click()");
+        
+		//alternativa pt isSelected()
+		String isSelected = jse.executeScript("return document.getElementById('wp-comment-cookies-consent').checked")
+				.toString();
+		
+		System.out.println(isSelected);
+		
+		//alternativa pt isEnabled ()
+		
+		String isEnabled = jse.executeScript("return document.getElementById('author').disabled")
+				.toString();
+		System.out.println(isEnabled);
+		
+	}
+	
+	@Test
+	public void example2() {
+		
+		JavascriptExecutor jse = (JavascriptExecutor) driver;
+		
+		String alertaJs = "window.obj = function(){alert('Alerta!!!')}; window.obj.call()";
+		
+		jse.executeScript(alertaJs);
+		//jse.executeScript
 	}
 	
 	
