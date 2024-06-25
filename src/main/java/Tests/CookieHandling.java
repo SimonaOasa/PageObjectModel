b4539011b39ddd1f32b2fa53b0ea77622b0c5418
simona oasa <simona.oasa@outlook.com>
2024-06-25 18:02:31 +0300

    curs32 window handle

---
+++ src/main/java/Tests/CookieHandling.java
@@ -0,0 +1,19 @@
+package Tests;
+
+import org.openqa.selenium.Cookie;
+import org.testng.annotations.Test;
+
+import SeleniumUtils.BaseTest;
+
+public class CookieHandling extends BaseTest {
+	
+	@Test(priority=1)
+	public void addCookie() {
+		
+		Cookie cookie = new Cookie("My custom cookie", "My super custom cookie value");
+		
+		driver.manage().addCookie(cookie);
+		//driver.manage().addCookie(new Cookie("My custom cookie", "My super custom cookie value"));
+	}
+
+}
--- src/main/java/Tests/WindowHandleExample.java
+++ src/main/java/Tests/WindowHandleExample.java
@@ -1,12 +1,19 @@
 package Tests;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.openqa.selenium.By;
 import org.testng.annotations.Test;
+
+import com.sun.tools.javac.util.List;
 
 import SeleniumUtils.BaseTest;
 
 public class WindowHandleExample extends BaseTest {
 	
 	
+	@SuppressWarnings("restriction")
 	@Test
 	public void windowHandleExample() {
 		
@@ -15,8 +22,57 @@ public class WindowHandleExample extends BaseTest {
 		
 		app.scrollVertically(800);
 		
+		System.out.println("Current window before tab: " + driver.getWindowHandle());
+		
+		System.out.println("All open windows : " + driver.getWindowHandles()); 
 		driver.switchTo().frame(app.getWebElement(app.genericEvent.mapFrame));
 		app.click(app.genericEvent.viewLargerMapLink);
+		
+		System.out.println(driver.getCurrentUrl());
+		System.out.println(driver.getTitle());
+		
+		System.out.println("----------------------------------");
+		
+		System.out.println("Current window: " + driver.getWindowHandle());
+		
+		System.out.println("All open windows after tab : " + driver.getWindowHandles());
+		
+		System.out.println("----------------------------------");
+		
+		//@SuppressWarnings("restriction")
+		List<String> browserTabs = new ArrayList<>(driver.getWindowHandles());
+		
+		driver.switchTo().window(browserTabs.get(1));
+		
+		System.out.println(driver.getCurrentUrl());
+		System.out.println(driver.getTitle());
+		
+		
+		driver.findElement(By.xpath("//span[text()='Accept all']")).click();
+		driver.findElement(By.id("searchboxinput")).clear();
+		driver.findElement(By.id("searchboxinput")).sendKeys("forest");
+		driver.findElement(By.id("searchbox-searchbutton")).click();
+		
+		
+		Thread.sleep(5000); 
+		
+		driver.close();
+		
+		Thread.sleep(2000);
+		
+		System.out.println("Current window : " + driver.getWindowHandle());
+		//
+		System.out.println("All open windows after tab : " + driver.getWindowHandles());
+		driver.switchTo().frame(app.getWebElement(app.genericEvent.mapFrame));
+		app.click(app.genericEvent.viewLargerMapLink);
+		//
+		
+		driver.switchTo().window(browserTabs.get(0));
+		
+		app.click(app.genericEvent.viewLargerMapLink);
+		
+		
+		
 	}
 	
 
